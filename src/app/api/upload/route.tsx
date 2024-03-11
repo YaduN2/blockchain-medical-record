7e6c4e44baa8631f9a62c473ebe4513bd8cf6eb3
Muaad-Akmal <=>
2024-03-12 02:58:36 +0530

    penting work : zip instead of base6

--- src/app/api/upload/route.tsx
+++ src/app/api/upload/route.tsx
@@ -1,4 +1,46 @@
-//TODO:
-1.save the file to server
-2. save it to ipfs 
-3. save the ipfs hash to blockchain 
+// //TODO:
+// 1.save the file to server
+// 2. save it to ipfs 
+// 3. save the ipfs hash to blockchain 
+import { NextResponse, NextRequest } from "next/server";
+
+// export const config = {
+//   api: {
+//     bodyParser: false,
+//   },
+// };
+
+export async function POST(request: NextRequest) {
+  try {
+    
+
+    // get the file from the formdata from request 
+    // const formData = await request.formData();
+    // const file = formData.get('file') as File;
+
+  
+    // send this file to ipfs so add this file to the body of the request
+
+
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIyZjI0ZTFhZC0wZGJhLTQ0OTEtOTI4My02YzMwODAxMWIyZDEiLCJlbWFpbCI6InJpcm9tYTM5MzZAaGRybG9nLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiI5MWRiMzM5NzI0ZDIxNDBiYWEzMiIsInNjb3BlZEtleVNlY3JldCI6ImQ4NTFlY2RjZWU2ZmVjY2RhZjYwNWM2OGU0MTk4NzRhODRlZjM4OTRjYzQ4ZmJiOTlkNThhM2FiNDI3ZjQ3ZDkiLCJpYXQiOjE3MTAxODYxNTJ9.i_3z6yOLn89MOxdcP_HChOwbrDLLskXmvQHSdPXrzAE',
+        'Content-Type': 'application/json'
+      },
+      body: '{"pinataContent":{"name":"samewose"}}'
+    };
+    
+    fetch('https://api.pinata.cloud/pinning/pinJSONToIPFS', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+
+  } catch (e) {
+    console.log(e);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
+}
--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -33,51 +33,51 @@ function FileUpload() {
       return
     }
 
-    let finalUploadObj = Object.fromEntries(formData.entries())
-    finalUploadObj.date = new Date().toISOString()
+    // let finalUploadObj = Object.fromEntries(formData.entries())
+    // finalUploadObj.date = new Date().toISOString()
 
-    const litNodeClient = new LitJsSdk.LitNodeClient({
-      litNetwork: "cayenne",
-    })
+    // const litNodeClient = new LitJsSdk.LitNodeClient({
+    //   litNetwork: "cayenne",
+    // })
 
-    await litNodeClient.connect()
-    const authSig = await LitJsSdk.checkAndSignAuthMessage({
-      chain: "ethereum",
-      nonce: "abcd",
-    })
+    // await litNodeClient.connect()
+    // const authSig = await LitJsSdk.checkAndSignAuthMessage({
+    //   chain: "ethereum",
+    //   nonce: "abcd",
+    // })
 
 
-    const access = [
-      {
-        contractAddress: " ",
-        standardContractType: " ",
-        chain: "ethereum",
-        method: "temp",
-        paramters: [":userAddress", "latest"],
-        returnValueTest: {
-          comparator: "===",
-          value: "0",
-        },
-      },
-    ]
+    // const access = [
+    //   {
+    //     contractAddress: " ",
+    //     standardContractType: " ",
+    //     chain: "ethereum",
+    //     method: "eth_getBalance",
+    //     paramters: [":userAddress", "latest"],
+    //     returnValueTest: {
+    //       comparator: ">=",
+    //       value: "0",
+    //     },
+    //   },
+    // ]
 
-    // TODO:
-    //in values we set to user address , then only user can decrypt the file
+    // // TODO:
+    // //in values we set to user address , then only user can decrypt the file
     
 
-    const encryptedZip = await LitJsSdk.encryptFileAndZipWithMetadata({
-      accessControlConditions: access,
-      authSig: authSig,
-      chain: "ethereum",
-      file: formData.get("file") as File,
-      readme: `${formData.get("description")} ${new Date().toISOString()}`,
-      litNodeClient: litNodeClient,
-    })
+    // const encryptedZip = await LitJsSdk.encryptFileAndZipWithMetadata({
+    //   accessControlConditions: access,
+    //   authSig: authSig,
+    //   chain: "ethereum",
+    //   file: formData.get("file") as File,
+    //   readme: `${formData.get("description")} ${new Date().toISOString()}`,
+    //   litNodeClient: litNodeClient,
+    // })
 
-    const encryptedBlob = new Blob([encryptedZip], { type: "text/plain" })
-    const encryptedFile = new File([encryptedBlob], "encrypted.zip")
+    // const encryptedBlob = new Blob([encryptedZip], { type: "text/plain" })
+    // const encryptedFile = new File([encryptedBlob], "encrypted.zip")
 
-    formData.set("file", encryptedFile)
+    // formData.set("file", encryptedFile)
  
   
 
@@ -95,20 +95,69 @@ function FileUpload() {
     let ipfsHash ="";
   
 
-    fetch("/api/upload", {
-      method: "POST",
-      body: formData,
-    })
-    .then((res) =>{
-      console.log("res", res);
-      return res.text();
-    }) // Add this line
-    .then((text) => {
-      ipfsHash = text;
-      setCid(ipfsHash);
-      console.log("cid", cid);
-    })
-    .catch((err) => console.log(err));
+    // fetch("/api/upload", {
+    //   method: "POST",
+    //   body: formData,
+    // })
+    // .then((res) =>{
+    //   console.log("res", res);
+    //   return res.text();
+    // }) // Add this line
+    // .then((text) => {
+    //   ipfsHash = text;
+    //   setCid(ipfsHash);
+    //   console.log("cid", cid);
+    // })
+    // .catch((err) => console.log(err))
+    // .finally(() => {
+    //   console.log("finally");
+    // }
+    // );
+
+// convert the file to base64 and then send it to the server 
+
+   console.log(file)
+
+  //  make base64 string of the file and send to ipfs
+
+  let reader = new FileReader();
+  reader.readAsDataURL(file);
+  let base64data = "";
+  reader.onloadend = function () {
+    let base64data = reader.result;
+    console.log(base64data);
+    console.log("file", file);
+  }
+
+  console.log("base64data", base64data.slice(0, 1000))
+
+  let body_ = {
+    pinataContent: {
+      name: "afgadf",
+      image: base64data.slice(0, 1000)
+    }
+  }
+
+
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySW5mb3JtYXRpb24iOnsiaWQiOiIyZjI0ZTFhZC0wZGJhLTQ0OTEtOTI4My02YzMwODAxMWIyZDEiLCJlbWFpbCI6InJpcm9tYTM5MzZAaGRybG9nLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjp0cnVlLCJwaW5fcG9saWN5Ijp7InJlZ2lvbnMiOlt7ImlkIjoiRlJBMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfSx7ImlkIjoiTllDMSIsImRlc2lyZWRSZXBsaWNhdGlvbkNvdW50IjoxfV0sInZlcnNpb24iOjF9LCJtZmFfZW5hYmxlZCI6ZmFsc2UsInN0YXR1cyI6IkFDVElWRSJ9LCJhdXRoZW50aWNhdGlvblR5cGUiOiJzY29wZWRLZXkiLCJzY29wZWRLZXlLZXkiOiI5MWRiMzM5NzI0ZDIxNDBiYWEzMiIsInNjb3BlZEtleVNlY3JldCI6ImQ4NTFlY2RjZWU2ZmVjY2RhZjYwNWM2OGU0MTk4NzRhODRlZjM4OTRjYzQ4ZmJiOTlkNThhM2FiNDI3ZjQ3ZDkiLCJpYXQiOjE3MTAxODYxNTJ9.i_3z6yOLn89MOxdcP_HChOwbrDLLskXmvQHSdPXrzAE',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body_)
+    };
+    
+    fetch('https://api.pinata.cloud/pinning/pinJSONToIPFS', options)
+      .then(response => response.json())
+      .then(response => console.log(response))
+      .catch(err => console.error(err));
+
+  }catch (e) {
+    console.log(e);
+  }
+
 
 
 }
