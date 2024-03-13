63ccb78e21455dbdf80f854d2a31566d6c8908a6
yadun2 <yyadunandann@gmail.com>
2024-03-13 16:25:33 +0530

    encryption and decryption finished - Testing required

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -3,132 +3,63 @@
 import React from "react";
 import styles from "@/styles/fileUpload.module.css";
 import Image from "next/image";
-import * as LitJsSdk from "@lit-protocol/lit-node-client";
 import { auth } from "@clerk/nextjs";
 import { useState } from "react";
 import axios from "axios";
+import * as LitJsSdk from "@lit-protocol/lit-node-client";
 
 function FileUpload() {
   const [cid, setCid] = React.useState<string>("");
   const [dycryptionCid, setDecryptionCid] = React.useState<string>("");
 
-  // const uploadIPFS = async () => {
-  //   const uploadURL = await upload({
-  //     data: [file],
-  //     options: {
-  //       uploadWithGatewayUrl: true,
-  //       uploadWithoutDirectory: true,
-  //     },
-  //   });
-  //   console.log("uploadURL", uploadURL);
-  // };
-
-  // function temp() {
-  //   return 1;
-  // }
-
-  // async function formHandler(e: React.FormEvent<HTMLFormElement>) {
-  //   e.preventDefault();
-
-  //   const formData = new FormData(e.currentTarget);
-  //   const description = formData.get("description") as string;
-  //   const file = formData.get("file") as File;
-
-  //   if (description === "" || file === null) {
-  //     alert("Please fill in the description and upload a file");
-  //     return;
-  //   }
-
-  //   //check file size
-  //   if (file.size > 1000000) {
-  //     alert("File size exceeds 1MB");
-  //     return;
-  //   }
-
-  //   let finalUploadObj = Object.fromEntries(formData.entries());
-  //   finalUploadObj.date = new Date().toISOString();
-
-  //   const litNodeClient = new LitJsSdk.LitNodeClient({
-  //     litNetwork: "cayenne",
-  //   });
-
-  //   await litNodeClient.connect();
-  //   const authSig = await LitJsSdk.checkAndSignAuthMessage({
-  //     chain: "ethereum",
-  //     nonce: "abcd",
-  //   });
-
-  //   const access = [
-  //     {
-  //       contractAddress: " ",
-  //       standardContractType: " ",
-  //       chain: "ethereum",
-  //       method: "temp",
-  //       paramters: [":userAddress", "latest"],
-  //       returnValueTest: {
-  //         comparator: "===",
-  //         value: "0",
-  //       },
-  //     },
-  //   ];
-
-  //   // TODO:
-  //   //in values we set to user address , then only user can decrypt the file
-
-  //   const encryptedZip = await LitJsSdk.encryptFileAndZipWithMetadata({
-  //     accessControlConditions: access,
-  //     authSig: authSig,
-  //     chain: "ethereum",
-  //     file: formData.get("file") as File,
-  //     readme: `${formData.get("description")} ${new Date().toISOString()}`,
-  //     litNodeClient: litNodeClient,
-  //   });
-
-  //   const encryptedBlob = new Blob([encryptedZip], { type: "text/plain" });
-  //   const encryptedFile = new File([encryptedBlob], "encrypted.zip");
-
-  //   formData.set("file", encryptedFile);
-
-  //   // before sentind the data to remote server
-
-  //   // save the file locally in the current directory
-
-  //   // const reader = new FileReader()
-  //   // reader.readAsDataURL(encryptedFile)
-  //   // reader.onloadend = function () {
-  //   //   const base64data = reader.result
-  //   //   console.log(base64data)
-  //   // }
-
-  //   let ipfsHash = "";
-
-  //   // const response_fetch = await fetch("/api/upload", {
-  //   //   method: "POST",
-  //   //   body: formData,
-  //   // })
-  //   //   .then((res) => {
-  //   //     console.log("res", res);
-  //   //     console.log(formData.get("file"));
-  //   //     return res.text();
-  //   //   }) // Add this line
-  //   //   .then((text) => {
-  //   //     ipfsHash = text;
-  //   //     setCid(ipfsHash);
-  //   //     console.log("cid", cid);
-  //   //   })
-  //   //   .catch((err) => console.log(err));
-  // }
-
-  // -------------------------decryption of the file from cid----------------------
-
-  const [fileImg, setFileImg] = useState(null);
+  const [fileToUpload, setfileToUpload] = useState(null);
 
   const sendFileToIPFS = async (e: any) => {
     e.preventDefault();
-    if (fileImg) {
+    if (fileToUpload) {
       try {
+        const litNodeClient = new LitJsSdk.LitNodeClient({
+          litNetwork: "cayenne",
+        });
+        // Then get the authSig
+        await litNodeClient.connect();
+        const authSig = await LitJsSdk.checkAndSignAuthMessage({
+          chain: "ethereum",
+          nonce: "abcdefghijkl",
+        });
+
+        const accs = [
+          {
+            contractAddress: "",
+            standardContractType: "",
+            chain: "ethereum",
+            method: "",
+            parameters: [":userAddress"],
+            returnValueTest: {
+              comparator: "=",
+              value: "0x88Eeba39524a63e316D02DC21DC5F0F234c638e2",
+            },
+          },
+        ];
+
+        const encryptedZip = await LitJsSdk.encryptFileAndZipWithMetadata({
+          accessControlConditions: accs,
+          authSig,
+          chain: "ethereum",
+          file: fileToUpload,
+          litNodeClient: litNodeClient,
+          readme: "Use IPFS CID of this file to decrypt it",
+        });
+
+        // Then we turn it into a file that will be accepted by the Pinata API
+        const encryptedBlob = new Blob([encryptedZip], {
+          type: "text/plain",
+        });
+
+        const encryptedFile = new File([encryptedBlob], fileToUpload.name);
+
         const formData = new FormData();
-        formData.append("file", fileImg);
+        formData.append("file", encryptedFile);
 
         const resFile = await axios({
           method: "post",
@@ -148,6 +79,45 @@ function FileUpload() {
         console.log("Error sending File to IPFS: ");
         console.log(error);
       }
+    }
+  };
+
+  const decryptFile = async (fileToDecrypt) => {
+    try {
+      // First we fetch the file from IPFS using the CID and our Gateway URL, then turn it into a blob
+      const fileRes = await fetch(
+        `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/${fileToDecrypt}?filename=encrypted.zip`
+      );
+      const file = await fileRes.blob();
+      // We recreated the litNodeClient and the authSig
+      const litNodeClient = new LitJsSdk.LitNodeClient({
+        litNetwork: "cayenne",
+      });
+      await litNodeClient.connect();
+      const authSig = await LitJsSdk.checkAndSignAuthMessage({
+        chain: "ethereum",
+        nonce: "abcdefghijkl",
+      });
+      // Then we simpyl extract the file and metadata from the zip
+      // We could do more with this, like try to display it in the app UI if we wanted to
+      console.log(authSig);
+      const { decryptedFile, metadata } =
+        await LitJsSdk.decryptZipFileWithMetadata({
+          file: file,
+          litNodeClient: litNodeClient,
+          authSig: authSig,
+        });
+      // After we have our dcypted file we can download it
+      const blob = new Blob([decryptedFile], {
+        type: "application/octet-stream",
+      });
+      const downloadLink = document.createElement("a");
+      downloadLink.href = URL.createObjectURL(blob);
+      downloadLink.download = metadata.name;
+      downloadLink.click(); // Use the metadata to get the file name and type
+    } catch (error) {
+      alert("Trouble decrypting file");
+      console.log(error);
     }
   };
 
@@ -172,11 +142,15 @@ function FileUpload() {
             name="file"
             id="file"
             className={styles.fileInput}
-            onChange={(e) => setFileImg(e.target.files[0])}
+            onChange={(e) => setfileToUpload(e.target.files[0])}
           />
           <input type="submit" value="Submit" className={styles.submitBtn} />
         </div>
       </form>
+      <div>
+        <input type="text" onChange={(e) => setDecryptionCid(e.target.value)} />
+        <button onClick={() => decryptFile(dycryptionCid)}>Decrypt</button>
+      </div>
     </div>
   );
 }
